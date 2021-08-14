4a3cde2f46a0e7629e97c0715bcd4c14d9d61c14
Thales Pessoa <pessoathales@outlook.com>
2021-08-14 18:22:38 -0300

    Do zero até o singleton

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,10 @@
+interface ICreateSpecificationDTO {
+    name: string;
+    description: string;
+}
+
+interface ISpecificationsRepository {
+    create({ description, name }: ICreateSpecificationDTO): void;
+    findByName(name: string);
+}
+export { ISpecificationsRepository, ICreateSpecificationDTO };
---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,22 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRquest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+    execute({ name, description }: IRquest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+        if (specificationAlreadyExists) {
+            throw new Error("Specification already exists!");
+        }
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
+    }
+}
+export { CreateSpecificationService };
